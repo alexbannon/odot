228f0b2da875a4909d18bd1ff930934031e46147
Alex Bannon <alex.bannon@mobileposse.com>
2017-05-07 16:39:19 -0400

    more rest functionality for todo_items and lists

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,5 +1,28 @@
 class TodoListsController < ApplicationController
   def index
+    @todo_lists = TodoList.all
+  end
+  def show
+    @todo_list = TodoList.find(params[:id])
+  end
+  def new
+    @todo_list = TodoList.new
+  end
+  def edit
+    @todo_list = TodoList.find(params[:id])
+  end
+  def create
+    @todo_list = TodoList.new(todo_list_params)
+    if @todo_list.save
+      redirect_to @todo_list
+    else
 
+    end
+  end
+
+  private
+
+  def todo_list_params
+    params.require(:todo_list).permit(:title, :description)
   end
 end
