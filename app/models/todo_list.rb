00454b32e747a2ccff0850e4eb45b303fb205649
Alex Bannon <alex.bannon@mobileposse.com>
2017-05-07 22:12:03 -0400

    testing validations

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,5 +1,6 @@
 class TodoList < ActiveRecord::Base
   has_many :todo_items
   validates :title, presence: true, length: { in: 1..60 }
+  validates :description, length: { in: 0..120 }
   scope :has_description, -> { where.not("description" => nil)}
 end
